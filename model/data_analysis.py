deeb3ddb29e5ccd751ed1c1cf3b8b8578de83146
katy.keenan@gmail.com <kek1@686kkmbp.local>
2020-10-07 22:48:17 -0600

    Generate ROC curve from bin files for calibration data

--- model/data_analysis.py
+++ model/data_analysis.py
@@ -1,6 +1,7 @@
 
 import glob
 import os
+import sys
 import time
 
 import matplotlib.pyplot as plt
@@ -8,6 +9,11 @@ import numpy as np
 import pandas as pd
 import plotly.graph_objs as go
 import plotly.express as px
+
+module_path = os.path.abspath(os.path.join('..'))
+if module_path not in sys.path:
+    sys.path.append(module_path)
+from model import en_convert
 
 DATA_PATH = '../data/raw'
 PLOT_PATH = '../resources/figures'
@@ -47,6 +53,91 @@ def load_data(fname):
     df['datetime'] = df['time'].astype('datetime64[s]')
     return df
 
+def load_cal(fname):
+    df = pd.read_csv(fname)
+    df['time'] = pd.to_datetime(df['time'])
+    # cleanup whitespaces in data
+    df = df.applymap(lambda x: x.strip() if type(x)==str else x)
+    # parse filename string for calibration test specifications
+    fname_parts = fname.split("_")
+    fname_cal_parts = fname_parts[-1].split(".")
+    cal_char = list(fname_cal_parts[0])
+    distance = float(''.join((cal_char[-3], '.', cal_char[-1])))
+    if len(cal_char) == 4:
+        cal_config = cal_char[0]
+    elif len(cal_char) == 5:
+        cal_config = ''.join((cal_char[0], cal_char[1]))
+    # add column distance based on filename string
+    df['distance'] = distance
+    # add column orientation based on filename string
+    df['cal_config'] = cal_config
+    # copy or rename mac to encounter_id
+    df['encounter_id'] = df['mac']
+    return df
+
+def load_cal_bin(fname, n_pts):
+    # parse filename to check cal config and distance
+    fname_parts = fname.split("_")
+    fname_cal_parts = fname_parts[-1].split(".")
+    cal_char = list(fname_cal_parts[0])
+    distance = float(''.join((cal_char[-3], '.', cal_char[-1])))
+    if len(cal_char) == 4:
+        cal_config = cal_char[0]
+    elif len(cal_char) == 5:
+        cal_config = ''.join((cal_char[0], cal_char[1]))
+
+    marks = en_convert.find_marks(fname)
+    datasets = {}
+    # DO need to loop over datasets to make the complete dataframe
+    for index in range(len(marks) - 1):
+        datasets[index] = {}
+        datasets[index]['header'], datasets[index]['data'] = en_convert.read_segment(fname, marks[index], marks[index+1])
+    # now have the header & data. need to form into a dataframe
+    # from the header, get epochtime, distance (now cm), orientation(was cal_config)
+    # need timestamp, channel, mac or encounter id
+    # “time since device boot”, “mac — a vector with 6 values”, “rssi”, “channel”, “rpi — a vector with 20 values”
+    # convert mac vector with 6 values to hexadecimal
+        i=0
+        device_time = np.empty(len(datasets[0]['data']))
+        mac = [None]*len(datasets[0]['data'])
+        rssi = np.empty(len(datasets[0]['data']))
+        ch = np.empty(len(datasets[0]['data']))
+
+        for row in datasets[index]['data']:
+            device_time[i] = row[0]
+            mac[i] = (bytes(row[1]).hex())
+            rssi[i] = row[2]
+            ch[i] = row[3]
+            i += 1
+
+    # make the dataframe
+        d = {'device_time':device_time, 'mac':mac, 'rssi':rssi, 'ch':ch}
+        df = pd.DataFrame(data=d)
+        if df['ch'][1] > 0:
+            df['ch'] = -df['ch']
+        df['epochtime'] = datasets[index]['header']['epochtime']
+        df['offsettime'] = datasets[index]['header']['offsettime']
+        df['offsetovflw'] = datasets[index]['header']['offsetovflw']
+        df['distance_header'] = datasets[index]['header']['distance']
+        df['distance_fname'] = distance*100 #make both distances in cm
+        df['cal_config'] = cal_config
+        df['orientation'] = datasets[index]['header']['orientation']
+        df['encounter_id'] = df['mac']
+        df['time'] = (df['device_time']-datasets[index]['header']['offsettime'])/1000 + datasets[index]['header']['epochtime'] - 6*3600
+        df['time'] = df['time'].astype('datetime64[s]')
+
+        # discard the first and last n data points
+        df.drop(df.head(n_pts).index, inplace = True)
+        df.drop(df.tail(n_pts).index, inplace = True)
+    return df
+
+def label_cal_data(df):
+    df['encounter_label'] = 1
+    for i, row in df.iterrows():
+        if row['distance'] >= 200: #now in cm
+            df.loc[i, 'encounter_label'] = 0
+    return df
+
 
 # Matplotlib plotting
 def plot_rssi(t, rssi):
@@ -80,7 +171,10 @@ def plot_rssi(t, rssi):
 # Plotly check all data plot
 # must input a df with time, rssi and encounter_id
 def plot_data_check(df):
-    fig = px.scatter(df, x="time", y="rssi", color="encounter_id", hover_data=["close encounter"])
+    if "close encounter" in df:
+        fig = px.scatter(df, x="time", y="rssi", color="encounter_id", hover_data=["close encounter"])
+    else:
+        fig = px.scatter(df, x="time", y="rssi", color="encounter_id")
     for trace in fig.data:
         trace.name = trace.name[:4]
     return fig
@@ -93,6 +187,18 @@ def plot_window_check(df, transitions):
         ax.axvspan(transitions[i], transitions[i+1], alpha=0.5)
     ax.set_ylim((-90, -40))
     return fig
+
+# plot the roc curve
+def plot_roc_curve_custom(fpr, tpr, rssi50, rssi60, WINDOW_SIZE, REQUIRED_READINGS):
+    plt.plot(fpr, tpr, 'co', label='ROC')
+    plt.plot([0, 1], [0, 1], color='grey', linestyle='--')
+    plt.plot(fpr[rssi60],tpr[rssi60],'ro', label='RSSI 60')
+    plt.plot(fpr[rssi50],tpr[rssi50],'ko', label='RSSI 50')
+    plt.xlabel('False Positive Rate')
+    plt.ylabel('True Positive Rate')
+    plt.title('MofN ROC: M=' + str(REQUIRED_READINGS) + ', N=' + str(WINDOW_SIZE))
+    plt.legend()
+    plt.show()
 
 #functions
 def get_unique(df):
@@ -117,11 +223,88 @@ def mofn_filter(df, window, readings, level=CLOSE_LEVEL):
     return df
 
 # notes transition between yes/no encounter and adds last data point as end transition, if needed
-def transitions(df):
+def get_transitions(df):
     transitions = df[df['encounter_transition']].index[1:]
     if len(transitions)%2:
         transitions = pd.DatetimeIndex.append(transitions,df.index[-1:])
     return transitions
+
+def find_gaps(df):
+    i = 0
+    for eid, df_eid in df.groupby('encounter_id'):
+        df_eid['gap'] = df_eid['time'].diff() > pd.Timedelta(minutes=10)
+        df_eid['count'] = df_eid['gap'].map(lambda x: 1 if x else 0).cumsum()
+        df.loc[df['encounter_id']==eid, 'count'] = df_eid['count']
+        df.loc[df['encounter_id']==eid, 'eid_index'] = i
+        i += 1
+    df['trace'] = df[['encounter_id', 'count']].apply(lambda x: '_'.join((x[0][:4], str(x[1]))), axis=1)
+
+def count_traces(df):
+#     print(df['trace'].value_counts())
+    return len(df['trace'].unique())
+
+def plot_these_traces(df, fig, WINDOW_SIZE, REQD_READINGS, CLOSE_LEVEL, ylim0, ylim1, figcolors):
+    i=0
+    for trace, df_trace in df.groupby(['trace']):
+
+        plot_a_trace(df_trace, fig, i, WINDOW_SIZE, REQD_READINGS, CLOSE_LEVEL, ylim0, ylim1, figcolors)
+        i += 1
+    fig.show()
+
+def plot_a_trace(df_eid, fig, plot_index, WINDOW_SIZE, REQD_READINGS, CLOSE_LEVEL, ylim0, ylim1, figcolors):
+    eid_index = int(df_eid.head(1).squeeze()['eid_index'])
+    df_eid = mofn_filter(df_eid, WINDOW_SIZE, REQD_READINGS, CLOSE_LEVEL)
+    df_eid.set_index('time', inplace=True)
+    transitions = get_transitions(df_eid)
+
+    for i in range (0, len(transitions), 2):
+        fig.add_trace(go.Scatter(x=[transitions[i], transitions[i], transitions[i+1], transitions[i+1], transitions[i]],
+                                 y=[ylim0, ylim1, ylim1, ylim0, ylim0], mode="lines",
+                                 fill="toself",line_width=0, fillcolor=figcolors[eid_index], opacity = 0.5, showlegend=False),
+                     row=1, col=plot_index+1)
+
+    fig.add_trace(go.Scatter(x=df_eid.index, y=df_eid.rssi,
+                             mode="markers", marker_color=figcolors[eid_index], hovertext=df_eid.ch),
+                  row=1, col=plot_index+1)
+
+def roc_these_traces(df, WINDOW_SIZE, REQD_READINGS, close_level_min=0, close_level_max=-90, step=1):
+    fpr = np.zeros(len(range(close_level_min, close_level_max, -step)))
+    tpr = np.zeros(len(range(close_level_min, close_level_max, -step)))
+
+    for index, close_level in enumerate(range(close_level_min, close_level_max, -step)):
+        col = f'suspected_{close_level}'
+        for trace, df_trace in df.groupby(['trace']):
+            label_a_trace(df_trace, close_level, WINDOW_SIZE, REQD_READINGS)
+            df.loc[df['trace']==trace, 'suspected_encounter'] = df_trace['suspected_encounter'].values
+        fpr[index], real_neg = get_fpr(df[['suspected_encounter', 'encounter_label']])
+        tpr[index], real_pos = get_tpr(df[['suspected_encounter', 'encounter_label']])
+        data_check = real_neg/real_pos
+    return fpr, tpr, data_check
+
+def label_a_trace(df_eid, close_level, WINDOW_SIZE, REQD_READINGS):
+    df_eid = mofn_filter(df_eid, WINDOW_SIZE, REQD_READINGS, close_level)
+    df_eid.set_index('time', inplace=True)
+
+# generates false positive rate
+def get_fpr(df):
+    fp = 0
+    for i in range(0,len(df)):
+        if df.suspected_encounter[i] > df.encounter_label[i]:
+            fp = fp + 1
+    real_neg = np.sum(1-df.encounter_label)
+    fpr = fp/real_neg
+    return fpr, real_neg
+
+# generates true positive rate
+def get_tpr(df):
+    tp = 0
+    for i in range(0,len(df)):
+        if df.suspected_encounter[i] == 1 & df.encounter_label[i] == 1:
+            tp = tp + 1
+    real_pos = np.sum(df.encounter_label)
+    tpr = tp/real_pos
+    return tpr, real_pos
+
 
 
 if __name__=='__main__':
